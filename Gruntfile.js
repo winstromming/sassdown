765d60397bcc8cc0abfff3874179053a944e1858
Jesper Hills <jay@nopr.co>
2013-11-12 10:36:46 +0000

    Added local dependencies, Gruntfile and JSHint settings file

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+/*
+* sassdown
+* github.com/nopr/sassdown
+*
+* Copyright (c) 2013 Jesper Hills, contributors
+* Some rights reserved
+*/
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            example: ['test/example/styleguide/'],
+        },
+
+        // Configuration to be run (and then tested).
+        sassdown: {
+            // options: {
+            //     template_assets: 'source/styleguide/',
+            //     template_html: 'source/styleguide.hbs',
+            //     includes: 'source/site_includes.hbs'
+            // },
+            files: {
+                expand: true,
+                cwd: 'test/example/assets/sass/partials',
+                src: ['**/*.scss'],
+                dest: 'test/example/styleguide/'
+            }
+        },
+
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['clean', 'jshint', 'sassdown']);
+
+};

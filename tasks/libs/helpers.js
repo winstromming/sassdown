aca6e8a39c5a09a30258304698f280c33b6d5a94
Paul Austin <paul@fantasticthinking.com>
2014-01-28 16:54:02 +0000

    Added a new Handlebars helper to loop around every nth items in a group. From http://funkjedi.com/technology/412-every-nth-item-in-handlebars/

--- tasks/libs/helpers.js
+++ tasks/libs/helpers.js
@@ -20,5 +20,19 @@ exports.init = function () {
     Handlebars.registerHelper('if_path', function(arg, self) {
         if (this.path === arg) { return self.fn(); }
     });
+    Handlebars.registerHelper('grouped_each', function(every, context, options) {
+        var out = '', subcontext = [], i;
+        if (context && context.length > 0) {
+            for (i = 0; i < context.length; i++) {
+                if (i > 0 && i % every === 0) {
+                    out += options.fn(subcontext);
+                    subcontext = [];
+                }
+                subcontext.push(context[i]);
+            }
+            out += options.fn(subcontext);
+        }
+        return out;
+    });
 
 };

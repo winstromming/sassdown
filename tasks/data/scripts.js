9db77e09a87fbec0fec37e6aeb10c73c87f0ddd9
Kenneth Sundqvist <eviloatmeal@gmail.com>
2014-06-26 23:22:41 +0200

    Move the navigation folder toggling logic into the scripts file.

--- tasks/data/scripts.js
+++ tasks/data/scripts.js
@@ -1,8 +1,8 @@
 (function (d) {
     // Helper functions
 
-    function query (str) { return d.querySelector(str); }
-    function queryAll (str) { return Array.prototype.slice.call(d.querySelectorAll(str)); }
+    function query (str, context) { return (context || d).querySelector(str); }
+    function queryAll (str, context) { return Array.prototype.slice.call((context || d).querySelectorAll(str)); }
 
     // Insert the results
 
@@ -28,4 +28,26 @@
             iframe.height = iframe.contentWindow.document.body.scrollHeight;
         });
     };
+
+    // Toggling navigation folders
+
+    queryAll('#nav .heading').forEach(function (heading) {
+        // Find the link to the current page and mark it and its parent folder as active/open
+        var list = heading.nextElementSibling;
+        queryAll('a', list).some(function (link) {
+            var href = link.getAttribute('href').replace(/^[\.\/]*/, '');
+            if (window.location.pathname.match(new RegExp(href + '$'))) {
+                link.classList.add('is-active');
+                link.parentNode.parentNode.classList.add('is-open');
+                link.parentNode.parentNode.previousElementSibling.classList.add('is-open');
+                return true;
+            }
+        });
+
+        // Toggle a folder open/closed when its heading is clicked
+        heading.addEventListener('click', function () {
+            this.classList.toggle('is-open');
+            this.nextElementSibling.classList.toggle('is-open');
+        });
+    });
 }(document));
